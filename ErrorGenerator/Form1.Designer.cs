f23fa8f0ed088c97ede44ec56ac02c6bbae42026
Christopher <krzysiek-smaga@outlook.com>
2020-12-16 00:02:00 +0100

    Add files via upload

---
+++ ErrorGenerator/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿
+namespace ErrorGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.title = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.text = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttons = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.type = new System.Windows.Forms.ComboBox();
+            this.generate = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tytuł:";
+            // 
+            // title
+            // 
+            this.title.Location = new System.Drawing.Point(12, 29);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(253, 20);
+            this.title.TabIndex = 1;
+            this.title.Text = "Witaj, świecie!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Tekst:";
+            // 
+            // text
+            // 
+            this.text.Location = new System.Drawing.Point(12, 68);
+            this.text.Multiline = true;
+            this.text.Name = "text";
+            this.text.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.text.Size = new System.Drawing.Size(253, 176);
+            this.text.TabIndex = 3;
+            this.text.Text = "Jestem programem do tworzenia sztucznych błędów systemowych :) ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(268, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Przyciski:";
+            // 
+            // buttons
+            // 
+            this.buttons.FormattingEnabled = true;
+            this.buttons.Items.AddRange(new object[] {
+            "Przerwij, Ponów próbę, Ignoruj",
+            "OK",
+            "OK, Anuluj",
+            "Ponów, Anuluj",
+            "Tak, Nie",
+            "Tak, Nie, Anuluj"});
+            this.buttons.Location = new System.Drawing.Point(271, 28);
+            this.buttons.Name = "buttons";
+            this.buttons.Size = new System.Drawing.Size(140, 21);
+            this.buttons.TabIndex = 5;
+            this.buttons.Text = "OK";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(268, 52);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Typ błędu";
+            // 
+            // type
+            // 
+            this.type.FormattingEnabled = true;
+            this.type.Items.AddRange(new object[] {
+            "Błąd",
+            "Informacyjny",
+            "Ostrzegawczy",
+            "Pytajnik"});
+            this.type.Location = new System.Drawing.Point(271, 68);
+            this.type.Name = "type";
+            this.type.Size = new System.Drawing.Size(140, 21);
+            this.type.TabIndex = 7;
+            this.type.Text = "Informacyjny";
+            // 
+            // generate
+            // 
+            this.generate.Location = new System.Drawing.Point(271, 221);
+            this.generate.Name = "generate";
+            this.generate.Size = new System.Drawing.Size(140, 23);
+            this.generate.TabIndex = 10;
+            this.generate.Text = "Generuj";
+            this.generate.UseVisualStyleBackColor = true;
+            this.generate.Click += new System.EventHandler(this.generate_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 251);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(156, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Stworzone przez KrzysiekSiemv";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(175, 251);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(40, 13);
+            this.linkLabel1.TabIndex = 12;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "GitHub";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(421, 274);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.generate);
+            this.Controls.Add(this.type);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.buttons);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.title);
+            this.Controls.Add(this.text);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(437, 313);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(437, 313);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ErGen - Generator błędów";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox title;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox text;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox buttons;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox type;
+        private System.Windows.Forms.Button generate;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ ErrorGenerator/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ErrorGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void generate_Click(object sender, EventArgs e)
+        {
+            showMessage(title.Text, text.Text, buttons.SelectedIndex, type.SelectedIndex);
+        }
+
+        void showMessage(string title, string text, int buttons, int types)
+        {
+            var button = new MessageBoxButtons();
+            var type = new MessageBoxIcon();
+
+            #region Przyciski
+            if (buttons == 0)
+                button = MessageBoxButtons.AbortRetryIgnore;
+            else if (buttons == 1)
+                button = MessageBoxButtons.OK;
+            else if (buttons == 2)
+                button = MessageBoxButtons.OKCancel;
+            else if (buttons == 3)
+                button = MessageBoxButtons.RetryCancel;
+            else if (buttons == 4)
+                button = MessageBoxButtons.YesNo;
+            else if (buttons == 5)
+                button = MessageBoxButtons.YesNoCancel;
+            else
+                button = MessageBoxButtons.OK;
+            #endregion
+            #region Ikona
+            if (types == 0)
+                type = MessageBoxIcon.Error;
+            else if (types == 1)
+                type = MessageBoxIcon.Information;
+            else if (types == 2)
+                type = MessageBoxIcon.Warning;
+            else if (types == 3)
+                type = MessageBoxIcon.Question;
+            else
+                type = MessageBoxIcon.Information;
+            #endregion
+
+            MessageBox.Show(text, title, button, type);
+        }
+
+        private void linkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            linkLabel1.LinkVisited = true;
+            System.Diagnostics.Process.Start("https://github.com/KrzysiekSiemv");
+        }
+    }
+}
